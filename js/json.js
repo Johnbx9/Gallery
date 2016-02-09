867d5902b600600cea30c79b599f2b4efc911f6d
mkaisere323 <mkaiser323@gmail.com>
2016-02-09 13:19:34 -0500

    First Commit

---
+++ js/json.js
@@ -0,0 +1,174 @@
+var art = [
+	{
+		"tags": [
+			{"tag":"color"},
+			{"tag":"road"},
+			{"tag":"cars"},
+			{"tag":"background"}
+		],
+		"url":"rainbow-road.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"rainbow road, just like in mario kart"
+	},{
+		"tags": [
+			{"tag":"color"},
+			{"tag":"animals"},
+			{"tag":"headshot"},
+			{"tag":"zebra"},
+			{"tag":"background"}
+		],
+		"url":"zebra.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"What's better than a black and white zebra? A rainbow zebra, that's what."
+	},{
+		"tags": [
+			{"tag":"color"},
+			{"tag":"black"},
+			{"tag":"abstract"},
+			{"tag":"circle"},
+			{"tag":"background"}
+		],
+		"url":"circle_art.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":""
+	},{
+		"tags": [
+			{"tag":"color"},
+			{"tag":"nudity"},
+			{"tag":"background"}
+		],
+		"url":"rainbow-hair.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"Did you ever get so high that you dunked your head in paint?"
+	},{
+		"tags": [
+			{"tag":"color"},
+			{"tag":"van gogh"},
+			{"tag":"painting"},
+			{"tag":"classic"}
+		],
+		"url":"starry-night.jpg",
+		"price":5000,
+		"artist":"van gogh",
+		"description":"goghing once. goghing twice. sold!!"
+	},{
+		"tags": [
+			{"tag":"city"},
+			{"tag":"color"},
+			{"tag":"black"},
+			{"tag":"fog"},
+			{"tag":"moon"},
+			{"tag":"sky"},
+			{"tag":"background"}
+		],
+		"url":"city_sky.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"will anyone even read these"
+	},{
+		"tags": [
+			{"tag":"clouds"},
+			{"tag":"landscape"},
+			{"tag":"sky"},
+			{"tag":"mountain"},
+			{"tag":"landscape"},
+			{"tag":"background"}
+		],
+		"url":"clouds_landscape.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"insert description here"
+	},{
+		"tags": [
+			{"tag":"city"},
+			{"tag":"green"},
+			{"tag":"black"},
+			{"tag":"spooky"},
+			{"tag":"apocalypse"},
+			{"tag":"end of days"},
+			{"tag":"aliens"},
+			{"tag":"run"},
+			{"tag":"background"}
+		],
+		"url":"greenishcity.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"spooky"
+	},{
+		"tags": [
+			{"tag":"moon"},
+			{"tag":"sky"},
+			{"tag":"clouds"},
+			{"tag":"bird"},
+			{"tag":"art"},
+			{"tag":"green"}
+		],
+		"url":"moon-landscape.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"why is the moon so big"
+	},{
+		"tags": [
+			{"tag":"mountains"},
+			{"tag":"ocean"},
+			{"tag":"sky"},
+			{"tag":"landscape"},
+			{"tag":"background"}
+		],
+		"url":"ocean_mountains.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":""
+	},{
+		"tags": [
+			{"tag":"black"},
+			{"tag":"moon"},
+			{"tag":"blue"},
+			{"tag":"sky"},
+			{"tag":"night"},
+			{"tag":"raven"}
+		],
+		"url":"raven_moon.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"creepy ass bird"
+	},{
+		"tags": [
+			{"tag":"robot"},
+			{"tag":"black and white"},
+			{"tag":"background"}
+		],
+		"url":"robot.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":""
+	},{
+		"tags": [
+			{"tag":"blue"},
+			{"tag":"whirlpool"},
+			{"tag":"ocean"},
+			{"tag":"background"}
+		],
+		"url":"whirlpool.jpg",
+		"price":5000,
+		"artist":"unknown",
+		"description":"downward spiral, just like my life"
+	},{
+		"tags": [
+			{"tag":"colors"},
+			{"tag":"shapes"},
+			{"tag":"abstract"},
+			{"tag":"wtf even is this"},
+			{"tag":"kindergarten"},
+			{"tag":"abstract"}
+		],
+		"url":"wtfevenisthis.jpg",
+		"price":5000,
+		"artist":"some 5 year old probably",
+		"description":"seriously what is this"
+	}
+]
